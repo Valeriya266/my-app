4bdfe8ef6c511cc9495232c870ef75ee11a72ad3
valeriya <valeriyastoyan26@gmail.com>
2024-04-27 21:00:22 +0300

    create counter

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ const reducer = (state = 0, action) => {
   switch (action.type) {
     case 'INC':
       return state + 1;
+    case 'DEC':
+      return state - 1;  
     default: 
       return state;
 
@@ -17,14 +19,19 @@ const reducer = (state = 0, action) => {
 
 const store = createStore(reducer);
 
-store.subscribe(() => {
-  console.log(store.getState())
+const update = () => {
+  document.getElementById('counter').textContent = store.getState();
+}
+
+store.subscribe(update);
+
+document.getElementById('inc').addEventListener('click', () => {
+  store.dispatch({type: 'INC'});
 })
 
-store.dispatch({type: 'INC'});
-store.dispatch({type: 'INC'});
-
-
+document.getElementById('dec').addEventListener('click', () => {
+  store.dispatch({type: 'DEC'});
+})
 
 console.log(store.getState());
 
