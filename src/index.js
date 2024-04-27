f4193ebe7443232a9ab25b2e781d27d1df8602eb
valeriya <valeriyastoyan26@gmail.com>
2024-04-27 13:38:39 +0300

    corr to React18

--- src/index.js
+++ src/index.js
@@ -13,12 +13,17 @@ const reducer = (state, action) => {
 let state = reducer(initialState, {type: 'INC'});
 console.log(state);
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root')
+);
+
+root.render(
   <React.StrictMode>
     <>
     </>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
 
 
