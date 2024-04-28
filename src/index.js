0aaa568a3599991402fb98b9b992ed30501878e0
valeriya <valeriyastoyan26@gmail.com>
2024-04-28 11:48:18 +0300

    add Provider and optimise

--- src/index.js
+++ src/index.js
@@ -2,37 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {createStore, bindActionCreators} from 'redux';
 import reducer from './reducer';
-import * as actions from './actions';
+import { Provider } from 'redux';
 
-import Counter from './components/Counter';
+import App from './components/App';
 
 const store = createStore(reducer);
 
-const {dispatch, subscribe, getState} = store;
-const {inc, dec, rnd} = bindActionCreators(actions, dispatch);
+const root = ReactDOM.createRoot(
+  document.getElementById('root')
+);
 
-const update = () => {
-  const root = ReactDOM.createRoot(
-    document.getElementById('root')
-  );
-  
-  root.render(
-    <React.StrictMode>
-      <Counter
-          counter={getState().value} 
-          inc={inc}
-          dec={dec}
-          rnd={() => {
-            const value = Math.floor(Math.random() * 10);
-            rnd(value);
-          }}   
-      />
-    </React.StrictMode>
-  );s
-}
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App/>
+    </Provider>
+    <App/>
+  </React.StrictMode>
+);
 
-update();
-subscribe(update);
+
 
 
 
