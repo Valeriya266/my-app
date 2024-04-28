3f45fa7c428c91bae4e3fc08e9686aea08879602
valeriya <valeriyastoyan26@gmail.com>
2024-04-28 10:44:16 +0300

    add props to Counter

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ const store = createStore(reducer);
 
 const {dispatch, subscribe, getState} = store;
 
-const update = () => {
-  document.getElementById('counter').textContent = getState().value;
-}
+//const update = () => {
+
+//}
 subscribe(update);
 
 //const bindActionCreator = (creator, dispatch) => (...args) => {
@@ -20,16 +20,14 @@ subscribe(update);
 
 const {inc, dec, rnd} = bindActionCreators(actions, dispatch);
  
-document.getElementById('inc').addEventListener('click', inc)
+//document.getElementById('inc').addEventListener('click', inc)
 
-document.getElementById('dec').addEventListener('click', dec)
+//document.getElementById('dec').addEventListener('click', dec)
 
-document.getElementById('rnd').addEventListener('click', () => {
-  const value = Math.floor(Math.random() * 10);
-  rnd(value);
-} )
-
-console.log(store.getState());
+//document.getElementById('rnd').addEventListener('click', () => {
+  //const value = Math.floor(Math.random() * 10);
+  //rnd(value);
+//} )
 
 
 const root = ReactDOM.createRoot(
